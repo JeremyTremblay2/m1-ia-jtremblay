0194ffee46500ea603c9d9f78279b013d735aa55
tremblay.jeremy <jeremy-tremblay@outlook.fr>
2023-10-01 15:54:06 +0200

    Add data_generation notebook for the TP1

---
+++ default.nix
@@ -0,0 +1,14 @@
+# shell.nix
+{ pkgs ? import <nixpkgs> {} }:
+let
+  my-python-packages = ps: with ps; [
+    pandas
+    requests
+    scikit-learn
+    numpy
+    jupyterlab
+    matplotlib
+    # other python packages
+  ];
+  my-python = pkgs.python3.withPackages my-python-packages;
+in my-python.env
